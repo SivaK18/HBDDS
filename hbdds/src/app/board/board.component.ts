67d09295dde4880302a8f191fd4c3eb4c80ad745
SivaK18 <sivakailash18@gmail.com>
2020-09-15 20:53:37 +0530

    the sample flip works

--- hbdds/src/app/board/board.component.ts
+++ hbdds/src/app/board/board.component.ts
@@ -5,11 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit {
 
+export class BoardComponent implements OnInit {
+  show_front : boolean;
   constructor() { }
 
   ngOnInit(): void {
+    this.show_front = false;
   }
-
+  flip() {
+    this.show_front = !this.show_front;
+    console.log(this.show_front);
+   
+}
 }
