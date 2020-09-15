c98b7988fc53ea6691c5009369e12c3a1bf63381
SivaK18 <sivakailash18@gmail.com>
2020-09-16 00:09:29 +0530

    needs css fixes

--- hbdds/src/app/app.module.ts
+++ hbdds/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GreetComponent } from './greet/greet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
+    GreetComponent
   ],
   imports: [
     BrowserModule,
---
+++ hbdds/src/app/greet/greet.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greet',
+  templateUrl: './greet.component.html',
+  styleUrls: ['./greet.component.scss']
+})
+export class GreetComponent implements OnInit {
+  show_front : boolean;
+  constructor() {  }
+
+  ngOnInit(): void {
+    show_front : true;
+  }
+  flip() {
+    this.show_front = !this.show_front;
+  }
+  
+   container_move(){
+    document.getElementById("shell").style.transform = document.getElementById("shell").style.transform == "translateX(0%)" ? "translateX(-50%)" : "translateX(0%)";
+    setTimeout("helo",500);
+    }
+  
+   open_card(){
+    console.log("called");
+    document.getElementById("container").style.transform = document.getElementById("container").style.transform == "rotateX(0deg)" ? "rotateX(70deg)" : "rotateX(0deg)";
+    document.getElementById("box1").style.transform = document.getElementById("box1").style.transform == "rotate3d(0, 1, 0, -180deg)" ? "rotate3d(0, 1, 0, 0deg)" : "rotate3d(0, 1, 0, -180deg)";
+    document.getElementById("box1document.getElementByIdback").style.transform = document.getElementById("box1document.getElementByIdback").style.transform == "rotate3d(0, 1, 0, -180deg)" ? "rotate3d(0, 1, 0, 0deg)" : "rotate3d(0, 1, 0, -180deg)";
+    document.getElementById("tn").innerHTML = document.getElementById("tn").innerHTML=="text" ? "surprise" : "text";
+    console.log("here");
+    }
+}
